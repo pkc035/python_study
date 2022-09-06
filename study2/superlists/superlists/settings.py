cab4773990f3dc05f7f55189f61219a3ae9dc718
pkc035 <pkc0305@gmail.com>
2022-09-07 07:47:34 +0900

    Add : gitignore

--- study2/superlists/superlists/settings.py
+++ study2/superlists/superlists/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from .my_settings import SECRET_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j=iu37+cpm7&7l&7y-=)n*bqnr243$#p@fnl2_snr+1k+6ck#*'
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
