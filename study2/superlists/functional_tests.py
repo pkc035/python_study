8e456511c6d2d4463e88507fae1e1aef10a1a70d
pkc035 <pkc0305@gmail.com>
2022-09-14 19:59:06 +0900

    feat : Template을 이용한 Home Page HTML 생성

--- study2/superlists/functional_tests.py
+++ study2/superlists/functional_tests.py
@@ -19,18 +19,19 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://127.0.0.1:8000')
         self.assertIn('To-Do', self.browser.title)
-        header_text = self.browser.find_element(By.TAG_NAME,"h1")
+        header_text = self.browser.find_element(By.TAG_NAME,"h1").text
+        print(header_text)
         self.assertIn('To-Do', header_text)
         
-        inputbox = self.browser.find_element_by_id('id_new_time')
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
         self.assertEqual(inputbox.get_attribute('placeholder'), '작업 아이템 입력')
 
         inputbox.send_keys('공작깃털 사기')
         inputbox.send_keys(Keys.ENTER)
 
-        table = self.browser.find_elemnet_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(any(row.text == '1: 공작깃털 사기' for row in rows),)
+        table = self.browser.find_element(By.ID,'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertTrue(any(row.text == '1: 공작깃털 사기' for row in rows), "신규 작업이 테이블에 표시되지 않는다.")
 
         self.fail('Finish the test!')
 
