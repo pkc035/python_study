6313b333a4eb155f909ca321878f13c3df9a520e
pkc035 <pkc0305@gmail.com>
2022-09-14 19:46:20 +0900

    refactor : home_page func, Templates 사용

--- study2/superlists/functional_tests.py
+++ study2/superlists/functional_tests.py
@@ -1,9 +1,14 @@
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
 import unittest
+
+from selenium                       import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by   import By
+from webdriver_manager.chrome       import ChromeDriverManager
+
 
 
 class NewVisitorTest(unittest.TestCase):
+
     def setUp(self):
         self.browser = webdriver.Chrome(ChromeDriverManager().install())
         self.browser.implicitly_wait(3) # 암묵적 대기
@@ -14,8 +19,20 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://127.0.0.1:8000')
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element(By.TAG_NAME,"h1")
+        self.assertIn('To-Do', header_text)
+        
+        inputbox = self.browser.find_element_by_id('id_new_time')
+        self.assertEqual(inputbox.get_attribute('placeholder'), '작업 아이템 입력')
 
+        inputbox.send_keys('공작깃털 사기')
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_elemnet_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(any(row.text == '1: 공작깃털 사기' for row in rows),)
+
+        self.fail('Finish the test!')
 
 if __name__  == '__main__':
     unittest.main(warnings='ignore')
