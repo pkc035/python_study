1c1e3b8fb7ffcbb065257b0561d2b29c4f8a1195
pkc035 <pkc0305@gmail.com>
2022-09-14 18:59:53 +0900

    feat : home_page func  Test

--- study2/superlists/lists/tests.py
+++ study2/superlists/lists/tests.py
@@ -1,6 +1,10 @@
+from urllib import response
 from django.test import TestCase
 from django.urls import resolve
+from django.http import HttpRequest
+
 from lists.views import home_page
+
 
 # class SmokeTest(TestCase):
 #     def test_bad_maths(self):
@@ -10,3 +14,10 @@ class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+    
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>To-Do lists</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
