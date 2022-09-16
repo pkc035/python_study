76ca7acf05115fe68b5ac8d611bd5487893a98f9
pkc035 <pkc0305@gmail.com>
2022-09-16 16:29:01 +0900

    feat : POST 요청 및 ORM 사용

--- study2/superlists/lists/tests.py
+++ study2/superlists/lists/tests.py
@@ -5,7 +5,8 @@ from django.urls import resolve
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 
-from lists.views import home_page
+from .views  import home_page
+from .models import Item
 
 
 # class SmokeTest(TestCase):
@@ -13,34 +14,65 @@ from lists.views import home_page
 #         self.assertEqual(1+1,3)
 
 class HomePageTest(TestCase):
+
     def test_root_url_resolves_to_home_view(self):
         found = resolve('/')
+
         self.assertEqual(found.func, home_page)
     
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-
-        # self.assertTrue(response.content.startswith(b'<html>'))
-        # self.assertIn(b'<title>To-Do lists</title>', response.content)
-        # self.assertTrue(response.content.endswith(b'</html>'))
-
-        # self.assertTrue(response.content.strip().endswith(b'</html>'))
-
         expected_html = render_to_string('home.html')
+
         self.assertEqual(response.content.decode(), expected_html)
 
     def test_home_page_can_save_a_POST_request(self):
         request = HttpRequest()
         request.method = 'POST'
         request.POST['item_text'] = "신규 작업 아이템"
-
         response = home_page(request)
 
+        self.assertEqual(Item.objects.count(), 1)
+
+        new_item = Item.objects.first().text
+
+        self.assertEqual(new_item, '신규 작업 아이템')
         self.assertIn("신규 작업 아이템", response.content.decode())
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
         expected_html = render_to_string(
             'home.html', {
                 'new_item_text' : '신규 작업 아이템'
             }
         )
+
         self.assertEqual(response.content.decode(), expected_html)
+    
+    def test_home_page_only_saves_items_when_necessary(self):
+        request = HttpRequest()
+        home_page(request)
+
+        self.assertEqual(Item.objects.count(), 0)
+
+
+class ItemModelTest(TestCase):
+
+    def test_saving_end_retrieving_items(self):
+        first_item = Item()
+        first_item.text = '첫 번째 아이템'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = '두 번째 아이템'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(),2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+
+        self.assertEqual(first_saved_item.text, '첫 번째 아이템')
+        self.assertEqual(second_saved_item.text, '두 번째 아이템')
--- study2/superlists/lists/views.py
+++ study2/superlists/lists/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http      import HttpResponse
+
+from .models import Item
 
 def home_page(request):
+    item = Item()
+
     if request.method == 'POST':
-        return render(request, 'home.html', {
-            'new_item_text' : request.POST.get('item_text', ''),
-        })
+        new_item_text = request.POST['item_text']
+        Item.objects.create(text=new_item_text)
+    else:
+        new_item_text = ''
+    
+    return render(request, 'home.html', {
+        'new_item_text' : new_item_text,
+    })
     
     # return HttpResponse('<html><title>To-Do lists</title></html>')
-    return render(request, 'home.html')
+    # return render(request, 'home.html')
 
