924ce53e822ab3cfc8f4dc189c61dc01e49680dd
pkc035 <pkc0305@gmail.com>
2022-09-16 16:47:01 +0900

    feat : POST 후 리디렉션과 템플릿에 작업 아이템 표시

--- study2/superlists/lists/views.py
+++ study2/superlists/lists/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http      import HttpResponse
 
 from .models import Item
@@ -7,15 +7,9 @@ def home_page(request):
     item = Item()
 
     if request.method == 'POST':
-        new_item_text = request.POST['item_text']
-        Item.objects.create(text=new_item_text)
-    else:
-        new_item_text = ''
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/')
     
-    return render(request, 'home.html', {
-        'new_item_text' : new_item_text,
-    })
-    
-    # return HttpResponse('<html><title>To-Do lists</title></html>')
-    # return render(request, 'home.html')
+    items = Item.objects.all()
+    return render(request, 'home.html', {'items' : items})
 
